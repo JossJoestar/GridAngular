662e44d2cefa53a47d996c81232a42f210a725b3
Oreos <oreoslawiet@gmail.com>
2021-07-03 16:59:31 -0700

    First configuration for the grid
    The configuration allows the image to be placed on the grid and it takes a maximum size of 900 pixels or adapts if it is smaller.
    The configuration allows you to select cells from the grid in the color black.
    Still other colors, sizes, or being able to reverse cells are not allowed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GridAngular';
+    title = 'GridAngular';
+    url = "https://www.stonewall.org.uk/sites/default/files/styles/basic_page_hero/public/trans_rights-logo_tw-post-1024x512.png?itok=XjhrJma7";
+    xLetters = false;
+    yLetters = true;
 }
